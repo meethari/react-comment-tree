36dd24a0564230c2434ed252da1a627c98778c4a
Hariharan Srinivasulu <srinivasulu.hari@gmail.com>
2022-01-28 23:27:10 -0800

    Added Post component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css'; 
-import Article from './components/article/Article';
+import Post from './components/post/Post';
 
 function App() {
   return (
     <div className="App">
-      <Article/>
+      <Post/>
     </div>
   );
 }
---
+++ src/components/post/Post.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './styles.css'
+import xboxImg from '../../images/xbox.jpg'
+
+export default function Post() {
+    return (
+        <div className="post">
+            <div className="post__votes">72</div>
+            <img className="post__img" src={xboxImg} alt="Xbox"></img>
+            <div className="post__text">
+                <div className="post__url">
+                    <a href="https://www.theverge.com/2022/1/18/22889258/microsoft-activision-blizzard-xbox-acquisition-call-of-duty-overwatch">Microsoft to acquire Activision in 67 billion dollar deal</a>
+                </div>
+                <div>
+                    <span>submitted 1 day ago by <a href="">user782</a></span>
+                </div>
+                <div>
+                    <a href="">100 comments</a>
+                    <span> </span>
+                    <a href="">share</a>
+                    <span> </span>
+                    <a href="">hide</a>
+                </div>
+            </div>
+        </div>
+    )
+}
