f432a991574d67b4135bbc4aa81e62a1b2c6cc49
Hariharan Srinivasulu <srinivasulu.hari@gmail.com>
2022-02-01 12:08:13 -0800

    Added Comment HTML and CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Post from "./components/post/Post";
+import Comment from "./components/comment/Comment";
 
 function App() {
   return (
     <div className="App">
       <Post />
+      <Comment />
     </div>
   );
 }
--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 import "./styles.css";
+import Upvotes from "../upvotes/Upvotes";
 
 export default function Comment(): JSX.Element {
-  return <div></div>;
+  return (
+    <div className="comment">
+      <div className="comment__votes">
+        <Upvotes />
+      </div>
+      <div className="comment__content">
+        <div className="comment__username">
+          <a href="">unidan</a>
+        </div>
+        <div className="comment__text">Wow, that was unexpected</div>
+        <div className="comment__links">
+          <a href="">reply</a>
+        </div>
+        <div className="comment__reply">...</div>
+        <div className="comment-list">tbd</div>
+      </div>
+    </div>
+  );
 }
