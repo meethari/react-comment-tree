2030a83c7934afd1b320000eef30ffd75b043344
Hariharan Srinivasulu <srinivasulu.hari@gmail.com>
2022-02-01 12:01:55 -0800

    Create files for Comment, CommentList, and ReplyInput components

---
+++ src/components/comment-list/CommentList.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./styles.css";
+
+export default function CommentList(): JSX.Element {
+  return <div></div>;
+}
---
+++ src/components/reply-input/ReplyInput.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./styles.css";
+
+export default function ReplyInput(): JSX.Element {
+  return <div></div>;
+}
