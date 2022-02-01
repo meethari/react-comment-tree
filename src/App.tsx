5dab9058ce984021caa7a72af39b63440a571597
Hariharan Srinivasulu <srinivasulu.hari@gmail.com>
2022-02-01 12:29:37 -0800

    Added ReplyInput HTML and CSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./App.css";
 import Post from "./components/post/Post";
 import Comment from "./components/comment/Comment";
+import ReplyInput from "./components/reply-input/ReplyInput";
 
 function App() {
   return (
     <div className="App">
       <Post />
+      <span>Comment:</span>
+      <br />
+      <ReplyInput />
       <Comment />
     </div>
   );
--- src/components/reply-input/ReplyInput.tsx
+++ src/components/reply-input/ReplyInput.tsx
@@ -2,5 +2,11 @@ import React from "react";
 import "./styles.css";
 
 export default function ReplyInput(): JSX.Element {
-  return <div></div>;
+  return (
+    <div className="reply">
+      <textarea className="reply__input" rows={6} cols={50}></textarea>
+      <br />
+      <button className="reply__submit">Submit</button>
+    </div>
+  );
 }
