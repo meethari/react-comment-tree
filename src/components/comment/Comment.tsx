724f5ae8607d487ced41105abc7a597f6f2a742f
Hariharan Srinivasulu <srinivasulu.hari@gmail.com>
2022-02-01 12:40:01 -0800

    Added ReplyInput to Comment

--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import Upvotes from "../upvotes/Upvotes";
+import ReplyInput from "../reply-input/ReplyInput";
 
 export default function Comment(): JSX.Element {
+  const [replyVisible, setReplyVisible] = useState(false);
+
+  const replyToggleHandler = (e: any) => {
+    e.preventDefault();
+    setReplyVisible(!replyVisible);
+  };
+
   return (
     <div className="comment">
       <div className="comment__votes">
@@ -14,9 +22,15 @@ export default function Comment(): JSX.Element {
         </div>
         <div className="comment__text">Wow, that was unexpected</div>
         <div className="comment__links">
-          <a href="">reply</a>
+          <a href="" onClick={replyToggleHandler}>
+            reply
+          </a>
         </div>
-        <div className="comment__reply">...</div>
+        {replyVisible && (
+          <div className="comment__reply">
+            <ReplyInput />
+          </div>
+        )}
         <div className="comment-list">tbd</div>
       </div>
     </div>
